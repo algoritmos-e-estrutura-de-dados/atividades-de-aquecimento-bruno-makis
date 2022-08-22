c62544496ec770ba175eba5469519502a151cffb
bruno-makis <110430914+bruno-makis@users.noreply.github.com>
2022-08-22 10:26:42 -0300

    adicionando exercicio13

---
+++ exercicios/area.py
@@ -0,0 +1,18 @@
+A=float(input("Digite a o primeiro número flutuante: "))
+B=float(input("Digite o segundo número flutuante: "))
+C=float(input("Digite o terceiro, e último, número flutuante: "))
+
+triangulo = ((A*C)/2)
+print(f"TRIANGULO: {triangulo:.3F}")
+
+circulo = ((3.14159)*(C**2))
+print(f"CIRCULO: {circulo:.3F}")
+
+trapezio = ((A+B)*(C)/2)
+print(f"TRAPEZIO: {trapezio:.3F}")
+
+quadrado = (B*B)
+print(f"QUADRADO: {quadrado:.3F}")
+
+retangulo = (A*B)
+print(f"RETANGULO: {retangulo:.3F}")
